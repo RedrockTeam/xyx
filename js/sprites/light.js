449f005ab006c13590e5a3767c08d962ad2f4d84
coderming <coderming@foxmail.com>
2018-04-12 17:39:39 +0800

    给光线增加随高度变化透明度的效果

--- js/sprites/light.js
+++ js/sprites/light.js
@@ -16,10 +16,26 @@ export default class Light {
     this.ctx = ctx
 
     this.light = light
+
+    twoWayBinding(this, 'y', dataBus, 'height')
   }
 
-
+  /**
+   * 实现了动态分辨率渲染
+   * 如果高度到了一定程度，就渐渐地更改透明度
+   */
   drawLight(ctx = this.ctx) {
+    let nowOpa = Math.max((1 - Math.max(this.y - 1400, 0) / 400), 0)
+    // 如果已经到了不用显示的地方，就不显示了
+    if (!nowOpa)
+      return void 0
+    // 如果还不需要变化opacity，就不用变
+    if (nowOpa === 1)
+      return this.light.draw(ctx)
+
+
+    ctx.globalAlpha = nowOpa
     this.light.draw(ctx)
+    ctx.globalAlpha = 1
   }
 }
